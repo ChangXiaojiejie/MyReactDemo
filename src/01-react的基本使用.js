1fc13eea999bf1eb23d0fd8612d55426cdaf7d6c
Aerfajj <15738665175@163.com>
2019-02-13 11:50:24 +0800

    第一天修改

--- src/01-react的基本使用.js
+++ src/01-react的基本使用.js
@@ -5,8 +5,13 @@
 import React from 'react'
 import ReactDom from 'react-dom'
 // 2.创建一个react的对象
-let element = React.createElement('div', {
-  id: 'box'
-}, "我是内容", '我是二号的内容')
+let element = React.createElement(
+  'div',
+  {
+    id: 'box'
+  },
+  '我是内容',
+  '我是二号的内容'
+)
 // 3. 将这个对象渲染出来
-ReactDom.render(element, document.getElementById("root"))
+ReactDom.render(element, document.getElementById('root'))
